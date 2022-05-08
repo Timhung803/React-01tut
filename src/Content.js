2f9e9250ea952f62f1c43512084b12f1b713568a
Timhung803 <timhkt@gmail.com>
2022-05-08 21:53:25 +1200

    Creating click events

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,25 @@
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ["Tim", "Vivi", "GG Master", "Wendy"];
-        const int = Math.floor(Math.random() * 4);
-        return names[int];
-      };
+  const handleNameChange = () => {
+    const names = ["Tim", "Vivi", "GG Master", "Wendy"];
+    const int = Math.floor(Math.random() * 4);
+    return names[int];
+  };
+
+  const handleClick = () => {
+      console.log('You Clicked it');
+  }
+  const handleClick2 = (name) => {
+      console.log(`${name} was clicked`);
+  }
+  const handleClick3 = (e) => {
+      console.log(e.target.innerText);
+  }
   return (
     <main>
-        <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2('Tim')}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
