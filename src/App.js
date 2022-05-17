28f078003695a1ad129e0536a639f5715a6e45dd
Timhung803 <timhkt@gmail.com>
2022-05-18 02:15:35 +1200

    Created Json server

--- src/App.js
+++ src/App.js
@@ -3,63 +3,63 @@ import SearchItem from "./SearchItem";
 import Additem from "./Additem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))|| []
+    );
 
-  const [newItem, setNewItem] = useState('')
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
 
-  const [search, setSearch] = useState('')
-
-  const setAndSaveItems = (newItem) => {
-    setItems(newItem);
-    localStorage.setItem("shoppinglist", JSON.stringify(newItem));
-  }
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items]);
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, item};
+    const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
-  }
+    setItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item);
-      setAndSaveItems(listItems);
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
     addItem(newItem);
-    setNewItem('');
-  }
+    setNewItem("");
+  };
 
   return (
     <div className="App">
       <Header title="Grocery List" />
-      <Additem 
+      <Additem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
-      <Content 
-      items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} 
-      handleCheck={handleCheck}
-      handleDelete={handleDelete}
-      />
-      <Footer length={items.length}/>
+      <Footer length={items.length} />
     </div>
   );
 }
